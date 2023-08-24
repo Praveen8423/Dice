f93c19bf65fb46437a5c2600e7d7ad926583c856
Praveen8423 <114797043+Praveen8423@users.noreply.github.com>
2023-08-24 23:18:36 +0530

    update

--- dice.py
+++ dice.py
@@ -1,29 +1,35 @@
-import csv
 import requests
+import csv
 from bs4 import BeautifulSoup
 
-# URL of the website to scrape
-url = 'https://www.dice.com'
+# Define the URL to scrape
+url = 'https://www.dice.com/'
 
-# Send an HTTP GET request
+# Send an HTTP GET request to the URL
 response = requests.get(url)
 
-# Check if the request was successful
+# if the request was successful
 if response.status_code == 200:
-    # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Extract job data
-    jobs = []
-    # Add your code here to extract job data and append it to the 'jobs' list
+    job_listings = soup.find_all('div', class_='job-listing')
 
-    # Store the data in a CSV file
-    with open('jobs.csv', 'w', newline='', encoding='utf-8') as csv_file:
-        fieldnames = ['Job Title', 'Description', 'Location', 'Company', 'Vendor', 'Posted Date', 'Job Type', 'Salary/Rate']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        for job in jobs:
-            writer.writerow(job)
+    csv_filename = 'dice_jobs.csv'
+    with open(csv_filename, 'w', newline='', encoding='utf-8') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['Job Title', 'Company', 'Location', 'Description', 'Posted Date'])
+
+        # Loop through each job listing and extract data
+        for job in job_listings:
+            job_title = job.find('h3', class_='card-title').text.strip()
+            company = job.find('span', class_='compName').text.strip()
+            location = job.find('span', class_='jobLoc').text.strip()
+            description = job.find('div', class_='card-description').text.strip()
+            posted_date = job.find('span', class_='posted').text.strip()
+
+            # Write the extracted data to the CSV file
+            csv_writer.writerow([job_title, company, location, description, posted_date])
+
+    print(f'Successfully scraped job data and saved to {csv_filename}')
 else:
     print('Failed to fetch the webpage.')
-
